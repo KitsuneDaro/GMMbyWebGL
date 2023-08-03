e235e2693d40dccffda4f70f5c61e7fe1c8c814d
KitsuneDaro <106896176+KitsuneDaro@users.noreply.github.com>
2023-08-03 15:05:49 +0900

    maybe final

--- src/GMM.ts
+++ src/GMM.ts
@@ -19,36 +19,30 @@ export class GMM {
 
     /* constructer input check */
 
-    static CheckMu(dist_n: number, mu: Float32Array) {
+    static CheckMu(dist_n: number, mu: Float32Array): boolean {
         return dist_n == mu.length;
     }
 
-    static CheckPi(dist_n: number, pi: Float32Array) {
+    static CheckPi(dist_n: number, pi: Float32Array): boolean {
         return dist_n == pi.length;
     }
 
-    static CheckSigma(dist_n: number, sigma: Float32Array) {
+    static CheckSigma(dist_n: number, sigma: Float32Array): boolean {
         return dist_n * dist_n == sigma.length;
-    }
-
-    // 過程を逐次見るデバッグ用
-    static NextButton() {
-
     }
 
     // GMMを作るやつ
     static CreateGMM(
-        dist_n: number, data_n: number, x: Float32Array, regularation_value: number = 0.0, log_p_tolerance_value: number = 1e-6
-    ): GMM {
+        dist_n: number, data_n: number, x: Float32Array, regularation_value: number = 0.0, max_iteration_time: number = 100, log_p_tolerance_value: number = 1e-6
+    ): {GMM: GMM, gamma: Float32Array, gamma_sum: Float32Array} {
 
         // Initialize
-        const x_mu_std = this.EvalXMuStd(data_n, x);
-        const x_mu = x_mu_std[0];
-        const x_std = x_mu_std[1];
+        const x_mu_var = this.EvalXMuVar(data_n, x);
+        const x_var = x_mu_var[1];
 
-        const init_mu = this.InitMu(dist_n, x_mu, x_std);
+        const init_mu = this.InitMu(dist_n);
         const init_pi = this.InitPi(dist_n);
-        const init_sigma = this.InitSigma(dist_n, x_std);
+        const init_sigma = this.InitSigma(dist_n, x_var);
 
         // Shaders
         // const inv_sigma_shader = 
@@ -307,13 +301,12 @@ export class GMM {
             }
         }
 
-        for(var i = 0; i < 100; i++) {
-            // to do
-            // 1. norm_x, norm_sum
+        // 1. norm_x, norm_sum
 
-            gpgpu.compute(norm_x_param);
-            gpgpu.compute(norm_x_sum_param);
-
+        gpgpu.compute(norm_x_param);
+        gpgpu.compute(norm_x_sum_param);
+        
+        for(var i = 0; i < max_iteration_time; i++) {
             // 2. gamma, gamma_sum
 
             gpgpu.compute(gamma_param);
@@ -324,7 +317,12 @@ export class GMM {
             gpgpu.compute(sigma_param);
             gpgpu.compute(mu_pi_param);
 
-            // 4. log_p, judge break
+            // 4. norm_x, norm_sum
+
+            gpgpu.compute(norm_x_param);
+            gpgpu.compute(norm_x_sum_param);
+
+            // 5. log_p, judge break
             
             log_p = log_p_func(norm_x_sum);
 
@@ -343,22 +341,27 @@ export class GMM {
             gpgpu.clear(mu_pi_param);
         }
 
-        return new GMM(dist_n, mu, pi, sigma);
+        return {
+            GMM: new GMM(dist_n, mu, pi, sigma),
+            gamma: gamma,
+            gamma_sum: gamma_sum
+        };
     }
 
-    static EvalXMuStd(data_n: number, x: Float32Array): Float32Array[] {
+    // データ点全体の平均と標準偏差を求める
+    static EvalXMuVar(data_n: number, x: Float32Array): Float32Array[] {
         const gpgpu = GPGPU.CreateGPGPU();
 
         const zero = new Float32Array(3);
         const x_mu = new Float32Array(3);
-        const x_std = new Float32Array(3);
+        const x_var = new Float32Array(3);
         
-        const x_mu_std_shader = `
+        const x_mu_var_shader = `
             uniform vec3 x[${data_n}];
 
             in vec3 zero;
             out vec3 x_mu;
-            out vec3 x_std;
+            out vec3 x_var;
 
             void main() {
                 x_mu = zero;
@@ -369,39 +372,151 @@ export class GMM {
 
                 x_mu /= ${data_n}.0;
 
-                x_std = zero;
+                x_var = zero;
 
                 for (int k = 0; k < ${data_n}; k++) {
                     vec3 nx = (x[k] - x_mu);
-                    x_std += nx * nx;
+                    x_var += nx * nx;
                 }
 
-                x_std /= ${data_n}.0;
-                x_std = sqrt(x_std);
+                x_var /= ${data_n}.0;
+                x_var = x_var;
             }
         `
 
-        const x_mu_std_param = {
-            id: 'x_mu_std_shader',
-            vertexShader: x_mu_std_shader,
+        const x_mu_var_param = {
+            id: 'x_mu_var_shader',
+            vertexShader: x_mu_var_shader,
             args: {
                 'x': x,
                 'zero': zero,
                 'x_mu': x_mu,
-                'x_std': x_std
+                'x_var': x_var
             }
         }
 
-        gpgpu.compute(x_mu_std_param);
-        gpgpu.clear(x_mu_std_param.id);
+        gpgpu.compute(x_mu_var_param);
+        gpgpu.clear(x_mu_var_param.id);
         
+        return [x_mu, x_var];
+    }
 
-        return [x_mu, x_std];
+    //　gammaから事後確率を計算
+    static PostProbByGamma(dist_n: number, data_n: number, gamma: Float32Array): Float32Array {
+        // Shaders
+        const gamma_sum_shader = `
+            uniform sampler2D gamma;
+            
+            in float zero;
+            out float gamma_sum;
+
+            void main() {
+                int n = gl_VertexID;
+                gamma_sum = zero;
+
+                for (int m = 0; m < ${dist_n}; m++) {
+                    gamma_sum += texelFetch(gamma, ivec2(n, m), 0).r;
+                }
+            }
+        `
+
+        const post_prob_shader = `
+            uniform float gamma_sum[${data_n}];
+
+            in float gamma;
+            out float post_prob;
+
+            void main() {
+                int n = gl_VertexID % ${data_n};
+                
+                post_prob = gamma / gamma_sum[n];
+            }
+        `;
+
+        // Variables
+        const gpgpu = GPGPU.CreateGPGPU();
+        const data_n_zero =  new Float32Array(data_n);
+        const gamma_sum = new Float32Array(data_n);
+        const post_prob = new Float32Array(dist_n * data_n);
+        
+        // Parameters
+        const gamma_sum_param = {
+            id: 'gamma_sum_shader',
+            vertexShader: gamma_sum_shader,
+            args: {
+                'gamma': gpgpu.makeTextureInfo('float', [dist_n, data_n], gamma),
+                'zero': data_n_zero,
+                'gamma_sum': gamma_sum
+            }
+        }
+
+        const post_prob_param = {
+            id: 'post_prob_shader',
+            vertexShader: post_prob_shader,
+            args: {
+                'gamma_sum': gamma_sum,
+                'gamma': gamma,
+                'post_prob': post_prob
+            }
+        }
+
+        gpgpu.compute(gamma_sum_param);
+        gpgpu.clear(gamma_sum_param.id);
+
+        gpgpu.compute(post_prob_param);
+        gpgpu.clear(post_prob_param.id);
+
+        return post_prob;
+    }
+
+    static ClusteringByPostProb(dist_n: number, data_n: number, post_prob: Float32Array): Float32Array {
+        // Shaders
+        const clustering_shader = `
+            uniform sampler2D post_prob;
+
+            in float zero;
+            out float x_cluster;
+
+            void main() {
+                int n = gl_VertexID;
+                
+                float max_post_prob = zero;
+
+                for (int m = 0; m < ${data_n}; m++) {
+                    float post_prob_n_m = texelFetch(post_prob, ivec2(n, m), 0).r;
+                    
+                    if (post_prob_n_m > max_post_prob) {
+                        max_post_prob = post_prob_n_m;
+                        x_cluster = m;
+                    }
+                }
+            }
+        `;
+
+        // Variables
+        const gpgpu = GPGPU.CreateGPGPU();
+        const data_n_zero = new Float32Array(data_n);
+        const x_cluster = new Float32Array(data_n);
+
+        // Parameters
+        const clustering_param = {
+            id: 'clustering_shader',
+            vertexShader: clustering_shader,
+            args: {
+                'post_prob': gpgpu.makeTextureInfo('float', [dist_n, data_n], post_prob),
+                'zero': data_n_zero,
+                'x_cluster': x_cluster
+            }
+        }
+
+        gpgpu.compute(clustering_param);
+        gpgpu.clear(clustering_param.id);
+
+        return x_cluster;
     }
 
     // 変数を初期化する
-    
-    static InitMu(dist_n: number, x_mu: Float32Array, x_std: Float32Array): Float32Array {
+    static InitMu(dist_n: number): Float32Array {
         const mu_x_cluster = kmeansInc(dist_n, data_n, x);
         const mu = mu_x_cluster[0];
 
@@ -409,9 +524,9 @@ export class GMM {
         // あんまりよくない初期値
         for (let i = 0; i< dist_n; i++) {
             let norm_value = this.Rnorm();
-            mu[0 + i * 3] = x_std[0] * norm_value + x_mu[0];
-            mu[1 + i * 3] = x_std[1] * norm_value + x_mu[1];
-            mu[2 + i * 3] = x_std[1] * norm_value + x_mu[2];
+            mu[0 + i * 3] = x_var[0] * norm_value + x_mu[0];
+            mu[1 + i * 3] = x_var[1] * norm_value + x_mu[1];
+            mu[2 + i * 3] = x_var[1] * norm_value + x_mu[2];
         }
         */
 
@@ -422,16 +537,16 @@ export class GMM {
         return new Float32Array(dist_n).fill(1.0 / dist_n);
     }
 
-    static InitSigma(dist_n: number, x_std: Float32Array): Float32Array {
-        const sigma = new Array<number>(9 * dist_n).fill(0.0);
+    static InitSigma(dist_n: number, x_var: Float32Array): Float32Array {
+        const sigma = new Float32Array(9 * dist_n).fill(0.0);
 
         for (let i = 0; i < dist_n; i++) {
-            sigma[0 + i * 9] = x_std[0];
-            sigma[4 + i * 9] = x_std[1];
-            sigma[8 + i * 9] = x_std[2];
+            sigma[0 + i * 9] = x_var[0];
+            sigma[4 + i * 9] = x_var[1];
+            sigma[8 + i * 9] = x_var[2];
         }
 
-        return new Float32Array(sigma);
+        return sigma;
     }
 
     // 標準正規分布の乱数(Box-Muller法)
